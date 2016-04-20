f6c6ea10b973da2030a9ece61e9bc712041687d0
Alma-Sanchez <alma08sanchez@gmail.com>
2016-04-20 19:33:57 -0400

    made sign up code look nice

--- signup.php
+++ signup.php
@@ -17,48 +17,50 @@
 	</head>
 
 	<body>
-		<div id="bannerarea">
-			<img src="nerdxing.jpg" alt="banner logo" /> <br />
-			where meek geeks meet
-		</div>
+		<?php
+	        include "common.php";
+	    ?>
+		<?= getHeader() ?>
+
 		<form action="signup">
-		<fieldset>
-			<legend>Sign up below:</legend>
-			Name: <input type="text" ></input><br>
-			Gender: <input type="radio" name="gender" value="female"></input>Female
-				<input type="radio" name="gender" value="male"></input>Male <br>
-				Age <select value="selectAge" >
-					<?php
-						for($i=18; $i<=79;$i++){
-							echo "<option value='$i'>$i</option>";
-						}
-					?>
-				</select>
-				<input type="text" ></input> <a href="https://www.16personalities.com/free-personality-test">(Don't know your type?)</a><br>
-				Favorite OS: <select>
-					<option value="windows">Windows</option>
-					<option value="Linux">Linux</option>
-					<option value="Mac">Mac</option>
-				</select>
-				<br>
-				Seeking age: 
-				<select value="minAge">	
-					<?php
-						for($i=18; $i<=79;$i++){
-							echo "<option value='$i'>$i</option>";
-						}
-					?>
-				</select>
-				to
-				<select value="maxAge">	
-					<?php
-						for($i=18; $i<=79;$i++){
-							echo "<option value='$i'>$i</option>";
-						}
-					?>
-				</select><br><br>
-				<input type="submit" value="Sign Up!" class="check-mates-btn"></input>
-		</fieldset>
+			<fieldset>
+				<legend>Sign up below:</legend>
+				Name: <input type="text" ></input><br>
+				Gender: <input type="radio" name="gender" value="female"></input>Female
+					<input type="radio" name="gender" value="male"></input>Male <br>
+					Age <select value="selectAge" >
+						<?php
+							for($i=18; $i<=79;$i++){
+								echo "<option value='$i'>$i</option>";
+							}
+						?>
+					</select>
+					<input type="text" ></input> <a href="https://www.16personalities.com/free-personality-test">(Don't know your type?)</a><br>
+					Favorite OS: <select>
+						<option value="windows">Windows</option>
+						<option value="Linux">Linux</option>
+						<option value="Mac">Mac</option>
+					</select>
+					<br>
+					Seeking age: 
+					<select value="minAge">	
+						<?php
+							for($i=18; $i<=79;$i++){
+								echo "<option value='$i'>$i</option>";
+							}
+						?>
+					</select>
+					to
+					<select value="maxAge">	
+						<?php
+							for($i=18; $i<=79;$i++){
+								echo "<option value='$i'>$i</option>";
+							}
+						?>
+					</select><br><br>
+					<input type="submit" value="Sign Up!" class="check-mates-btn"></input>
+			</fieldset>
 		</form>
+		<?= getFooter() ?>
 	</body>
 </html>
