ba6c8f81077a71d73a600c1fb2430f8bd7aeb3c7
Alex Neuenkirk <alex.neuenkirk@gmail.com>
2016-04-23 21:00:49 -0400

    Implemented find compatible matches, need to be displayed still.

---
+++ matches-submit.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<?php include "common.php" ?>
+    <?= getHead() ?>
+    <body>
+    <?= getHeader() ?>
+    
+    <h3>Matches for User</h3>
+    
+<?php
+//    echo $_SERVER["PHP_SELF"];
+//    echo "<br>";
+    $name = $_POST["fullName"];
+    $names = explode(" ", $name);
+    $userProfile = getUserProfile($name);
+    $matches = getMatches($userProfile);
+    
+    if (count($matches) > 0) {
+        // Render matches.
+    } else {
+        echo "No matches meet your criteria.";
+    }
+    
+    
+//    $singlesContents = fread($singlesHandler, 1000000);
+?>
+        
+    <?= getFooter() ?>
+    </body>
+</html>
