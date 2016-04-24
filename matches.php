e722a33a91d2c37f4cd67a8138253af274f56fe1
Alex Neuenkirk <alex.neuenkirk@gmail.com>
2016-04-23 21:37:50 -0400

    Changed method to post, changed input name to fullName.

--- matches.php
+++ matches.php
@@ -4,11 +4,11 @@
     <?= getHead() ?>
     <body>
         <?= getHeader() ?>
-        <form action="matches-submit.php" method="get">
+        <form action="matches-submit.php" method="post">
             <fieldset>
                 <legend>Returning User</legend>
                 Full name:
-                <input type="text" name="name"><br><br>
+                <input type="text" name="fullName"><br><br>
                 <input type="submit" value="View Your Matches">
             </fieldset>
         </form>
